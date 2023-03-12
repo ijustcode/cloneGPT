fee1a9d992229574b42321547d90ba79d7ea3295
Pouya Jafarian <pouya.jafarian@varian.com>
2023-03-12 01:36:43 -0800

    cleaned up the selection page

--- src/AIOptions/index.jsx
+++ src/AIOptions/index.jsx
@@ -2,7 +2,7 @@ export const arrayItems = [
     {
     name: 'Q&A',
     id: 'q&a',
-    description:'Answer questions based on existing knowledge',
+    description:'This one answers questions based on model\'s knowledge',
     option: {
         model: "text-davinci-003",
             temperature: 0,
@@ -14,9 +14,9 @@ export const arrayItems = [
         }
     },
     {
-        name: 'Explain it like I am 8 _Under Construction_',
-        id: 'eli8',
-        description:'Explain a complex topic in simple terms. _Doesn\'t work yet_',
+        name: 'Text-to-Image (not ready)',
+        id: 'tti',
+        description:'I\'ll add this later. Text-to-image service',
         option: {
             model: "text-davinci-003",
             temperature: 0.7,
@@ -24,20 +24,6 @@ export const arrayItems = [
             top_p: 1.0,
             frequency_penalty: 0.0,
             presence_penalty: 0.0,
-    }
-    },
-    {
-        name: 'code meaning _Under Construction_',
-        id: 'coder',
-        description:'explains code. _Doesn\'t work yet_',
-        option: {
-            model: "code-davinci-002",
-            temperature: 0,
-            max_tokens: 64,
-            top_p: 1.0,
-            frequency_penalty: 0.0,
-            presence_penalty: 0.0,
-            stop: ["\"\"\""],
         }
     }
 ]
